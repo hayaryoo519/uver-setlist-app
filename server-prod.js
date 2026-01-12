cdb181c3c704a1848f638da99856b11d700272fa
hayaryoo519 <oaulth_0117@yahoo.co.jp>
2026-01-13 00:28:18 +0900

    fix: Expressルーティング構文の修正

--- server-prod.js
+++ server-prod.js
@@ -14,8 +14,8 @@ app.use('/api', createProxyMiddleware({
 // Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// SPA fallback
-app.get('*', (req, res) => {
+// SPA fallback - must be last
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
