8a944254952ddbe2be48939064571d1ed87bfa04
hayaryoo519 <oaulth_0117@yahoo.co.jp>
2026-02-03 21:24:23 +0900

    Cleanup: 不要な開発スクリプト削除 + 内部IP情報の汎用化

--- server/add_qualia.js
+++
@@ -1,8 +0,0 @@
-const db = require('./db');
-
-db.query("INSERT INTO songs (title, album) VALUES ('Qualia', 'LIFE 6 SENSE') RETURNING *")
-    .then(r => {
-        console.log('Added:', r.rows[0]);
-        process.exit(0);
-    })
-    .catch(e => { console.error(e); process.exit(1); });
--- server/check_equal.js
+++
@@ -1,4 +0,0 @@
-const db = require('./db');
-db.query("SELECT id, title, album FROM songs WHERE title = '='")
-    .then(r => { console.log(r.rows); process.exit(0); })
-    .catch(e => { console.error(e); process.exit(1); });
