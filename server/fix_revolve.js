f43f94b07bd2b0431a12b738d7e5c90b75465059
hayaryoo519 <oaulth_0117@yahoo.co.jp>
2026-02-03 21:24:23 +0900

    Cleanup: 不要な開発スクリプト削除 + 内部IP情報の汎用化

--- server/fix_revolve.js
+++
@@ -1,4 +0,0 @@
-const db = require('./db');
-db.query("UPDATE songs SET album = 'Single' WHERE title = 'Revolve'")
-    .then(r => { console.log('Updated:', r.rowCount); process.exit(0); })
-    .catch(e => { console.error(e); process.exit(1); });
